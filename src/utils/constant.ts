ef995f174312ada3cc7bad0b0a6cee06da0ae1b3
kcanamar <99228411+kcanamar@users.noreply.github.com>
2024-11-23 18:01:31 -0800

    ui: settings page pre forms

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
  
 export const TOKEN = "Bearer";
-export const PUBLIC_ROUTES = ["/", "/404", "/error", "/api/login", "/api/logout", "/api/signup"];
+export const PUBLIC_ROUTES = ["/", "/404", "/error", "/signup", "/login",  "/api/login", "/api/logout", "/api/signup"];
 
 export const SECRET = import.meta.env.JWT_SECRET
 export const pSql = neon(import.meta.env.DATABASE_URL)
